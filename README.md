e9c3f6a0448cfe548148b175dccec6332a4c4261
addshore <addshorewiki@gmail.com>
2021-01-04 16:05:54 +0000

    Basic edit reconciliation endpoint for urls
    Still has some bugs, such as the statement guid issue.
    Still needs refactoring properly, testing and accepting
    of real requests rather than reading from disk.
    Other things like users, tokens etc also have to be done
    (There are various TODOs)

--- README.md
+++ README.md
@@ -1,52 +1,42 @@
 # OnOrProt
 
-OnOrProt is a set ofOpen!Next prototype APIs, that is also relevant to OpenRefine.
+OnOrProt contains Open!Next prototype APIs.
+These could also be relevant to OpenRefine and other project in the future.
 
-## 1) Easy Reconciliation
+## /onorprot/v0/edit (Editing API)
 
-Given "P123" -> "ABC123" give me the Item ID if it exists?
+Provides simple reconciliation of Item edits.
 
-The usecase for ON here would be simple reconciliation based upon "patent identifiers" as referenced in a meeting.
+### Reconciliation 0.0.1
 
-This would likely be backed with the Query Service (possibly with a check in PHP).
-In the future Elastic for example should also be able to be a backend?
+The default initial behavior is:
 
-## 2) Fancy editing
+* Merge: Labels, Descriptions, Aliases
+* Set: Statements
+* Not Supported: Sitelinks, Qualifiers, References, NoValues, SomeValues
 
-A few different general directions were considered:
+This means that:
 
-- **wikibase minimal**
-  - A more minimal version of an entity that might be useful?
-  - Similar to https://github.com/maxlath/wikibase-edit/blob/master/docs/how_to.md#entity
-- **jsonld**
-  - Exploration in the jsonld direction, but this might end up being far to complicated for that we need.
-  - Though we might be able to take learnings fron this for whatever we create?
-- **csv**
-  - Current primary investigation target
-  - Ultimately everything can be mapped to a CSV? and everything can be mapped from a CSV to Wikibase?
+* Labels, Descriptions, Aliases will never be removed, and any new values provided will be added or replace existing values
+* Statements that want to continue existing should ALWAYS be provided in the input.
 
-### General Flow
+Reconciliation payload 0.0.1 should look like this:
 
-- User provides data a simple format (such as CSV) and a schema that is decided by the API.
-- Other formats than CSV can be used? As long as the filetype can be mapped to a CSV?
-- The Schema file includes infomation mapping:
-  - Column -> part of a Wikibase entity
-  - Data upon which to reconcile (initially simple single statement match?)
-  - Other data needed to complete the edit?
-- The data and schema file is then split up into native Wikibase Items along with some DATA needed to make the edit?
-- The edits are then made?
+```js
+{
+    "version":"0.0.1",
+    "urlReconcile": "P23"
+}
+```
 
-### CSV to mapping
+### Edit 0.0.1
 
-Everything within an item is individually addressable (and also the current state is referencable?)
+Initially we use JSON that matches regular Entity serialization.
 
-- label/en - The en label regardless of current value
-- label/en@"Foo"- The en label with a value of "Foo"?
-- alias/en@"Foo" - The en alias that has a value of "Foo"?
-- statement/P123
-- TODO further down statements
-- sitelink/enwiki
+In future versions this will likely be stripped down.
 
-## 3) Batch async editing
+Edit payload 0.0.1 should look like this:
 
-This is the same as the level 2 API, however it has a batch interface, and throws all of the edits that need to be in a job etc.
+```js
+tba
+```
--- includes/MediaWiki/Api/EditEndpoint.php
+++ includes/MediaWiki/Api/EditEndpoint.php
@@ -2,62 +2,189 @@
 
 namespace MediaWiki\Extension\OnOrProt\MediaWiki\Api;
 
-use MediaWiki\Extension\OnOrProt\Model\Input;
-use MediaWiki\Extension\OnOrProt\Model\Schema;
-use MediaWiki\Extension\OnOrProt\Wikibase\ItemGenerator;
 use MediaWiki\Rest\SimpleHandler;
 use Wikimedia\ParamValidator\ParamValidator;
+use Wikibase\DataModel\Entity\PropertyId;
 use Wikibase\DataModel\Entity\Item;
+use Wikibase\Repo\WikibaseRepo;
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+use DataValues\StringValue;
+use MediaWiki\MediaWikiServices;
+use Wikibase\DataModel\Entity\ItemId;
 
 class EditEndpoint extends SimpleHandler {
 
+	private const VERSION_KEY = "onorprot-version";
+
 	public function run() {
-		// TODO use different Request interfaces
+		// TODO use different Request interfaces / real input data
+		$repo = WikibaseRepo::getDefaultInstance();
 		$request = new \MediaWiki\Extension\OnOrProt\MediaWiki\Request\MockEditDiskRequest();
-		$rawEntity = $request->entity( $this->getRequest() );
+
+		// Validate and process reconciliation input
+		// TODO use different services per version
+		// TODO output an object that controls the reconciliations spec?
 		$rawReconcile = $request->reconcile( $this->getRequest() );
-
-		var_dump($rawReconcile);
-		die();
-
-		/**
-		 * 1) INPUT to the API from user
-		 * Could be actual files, or could be URls?
-		 * Could also not be CSV, and we could provide automatic ways to transform other file types to CSV / whatever we use internally.
-		 */
-		$schema = new Schema( $rawSchema );
-		$input = new Input( $rawInput, $schema->csvHasHeaders );
-
-		/**
-		 * 2) MAP the input data to Wikibase Item / concpets
-		 * Might want properties too in the future?
-		 */
-		$itemGenerator = new ItemGenerator();
-		$items = $itemGenerator->generate( $schema, $input );
-
-		/**
-		 * 3) Perform reconciliation & set the ID if it is know?
-		 * Based on the infomation that is provided in the schema file.
-		 * This will initially be using the query service?
-		 * This might in the future include PHP based checks? and or Elastic search lookups if possible?
-		 */
-		// TODO
-
-		/**
-		 * 4) Make the edit
-		 * Either by a) creating a new entity?
-		 * b) Overriding what exists on the existing item with what was sent in the input?
-		 * TODO maybe strategy for this needs to be defined in the schema?
-		 */
-
-		/**
-		 * 5) Output stuff to the user?
-		 */
-		$rawOutput = [];
-		foreach ( $items as $item ) {
-			$rawOutput[] = var_export( $item, true );
+		$inputReconcile = json_decode( $rawReconcile, true );
+		if ( !array_key_exists( self::VERSION_KEY, $inputReconcile ) || $inputReconcile[self::VERSION_KEY] !== '0.0.1' ) {
+			die( 'Only supported reconciliation version is 0.0.1' );
 		}
-		return $rawOutput;
+		if ( !array_key_exists( 'urlReconcile', $inputReconcile ) || !preg_match( PropertyId::PATTERN, $inputReconcile['urlReconcile'] ) ) {
+			die( '0.0.1 requires a single urlReconcile key mapped to a property id, such as P123' );
+		}
+		$reconcileUrlProperty = new PropertyId( $inputReconcile['urlReconcile'] );
+		// For now this property must be of URL type
+		if ( $repo->getPropertyDataTypeLookup()->getDataTypeIdForProperty( $reconcileUrlProperty ) !== 'url' ) {
+			die( 'urlReconcile property must be of type url' );
+		}
+
+		// Validate entity input
+		$rawEntity = $request->entity( $this->getRequest() );
+		$inputEntity = json_decode( $rawEntity, true );
+		if ( !array_key_exists( self::VERSION_KEY, $inputEntity ) || $inputEntity[self::VERSION_KEY] !== '0.0.1' ) {
+			die( 'Only supported entity version is 0.0.1' );
+		}
+		if ( !array_key_exists( 'type', $inputEntity ) || $inputEntity['type'] !== 'item' ) {
+			die( 'Only supported entity type is \'item\'' );
+		}
+		$deserializer = $repo->getBaseDataModelDeserializerFactory()->newEntityDeserializer();
+		/** @var Item $inputEntity */
+		$inputEntity = $deserializer->deserialize( $inputEntity );
+
+		// Validate Entity
+		// Don't support references, qualifiers or sitelinks
+		foreach ( $inputEntity->getStatements()->toArray() as $statement ) {
+			if ( $statement->getQualifiers()->count() !== 0 || $statement->getReferences()->count() !== 0 ) {
+				die( 'Qualifiers and References are not currently supported' );
+			}
+		}
+		if ( $inputEntity->getSiteLinkList()->count() !== 0 ) {
+			die( 'Sitelinks are not currently supported' );
+		}
+		// Check for our reconciliation value
+		$reconciliationStatements = $inputEntity->getStatements()->getByPropertyId( $reconcileUrlProperty );
+		if ( $reconciliationStatements->count() !== 1 ) {
+			die( 'Entity must have at least one statement for the reconciliation Property' );
+		}
+		$reconciliationStatement = $reconciliationStatements->toArray()[0];
+		if ( !$reconciliationStatement->getMainSnak() instanceof PropertyValueSnak ) {
+			die( 'Reconciliation statement must be of type value ' );
+		}
+		/** @var PropertyValueSnak $reconciliationMainSnak */
+		$reconciliationMainSnak = $reconciliationStatement->getMainSnak();
+		/** @var StringValue $reconciliationDataValue */
+		$reconciliationDataValue = $reconciliationMainSnak->getDataValue();
+		$reconciliationUrl = $reconciliationDataValue->getValue();
+
+		// Is this URL in the externallinks table?
+		$externalLinkIndexes = \LinkFilter::makeIndexes( $reconciliationUrl );
+		if ( count( $externalLinkIndexes ) !== 1 ) {
+			die( 'Unexpected issue with LinkFilter return' );
+		}
+		$externalLinkIndex = $externalLinkIndexes[0];
+		$db = MediaWikiServices::getInstance()->getDBLoadBalancer()->getConnection( DB_REPLICA );
+		// TODO join and query correct ns only etc?
+		$externalLinksResult = $db->select(
+			'externallinks',
+			'el_from',
+			[
+				'el_index' => $externalLinkIndex
+			],
+			__METHOD__
+		);
+		// Find ItemIds from the external link matches
+		$itemIdsThatReferenceTheUrl = [];
+		foreach ( $externalLinksResult as $row ) {
+			$pageId = $row->el_from;
+			$title = \Title::newFromID( $pageId );
+			$inputEntityId = $repo->getEntityIdLookup()->getEntityIdForTitle( $title );
+			if ( $inputEntityId && $inputEntityId instanceof ItemId ) {
+				$itemIdsThatReferenceTheUrl[] = $inputEntityId;
+			}
+		}
+		// Find Items that match the URL and Property ID
+		$itemsThatReferenceTheUrlInCorrectStatement = [];
+		foreach ( $itemIdsThatReferenceTheUrl as $itemId ) {
+			/** @var Item $item */
+			$item = $repo->getEntityLookup()->getEntity( $itemId );
+			foreach ( $item->getStatements()->getByPropertyId( $reconcileUrlProperty )->toArray() as $statement ) {
+				if ( !$statement->getMainSnak() instanceof PropertyValueSnak ) {
+					continue;
+				}
+				/** @var PropertyValueSnak $mainSnak */
+				$mainSnak = $statement->getMainSnak();
+				$urlOfStatement = $mainSnak->getDataValue()->getValue();
+				if ( $urlOfStatement === $reconciliationUrl ) {
+					$itemsThatReferenceTheUrlInCorrectStatement[] = $item;
+				}
+			}
+		}
+
+		// If we have more than one item matches, something is wrong and we can't edit
+		if ( count( $itemsThatReferenceTheUrlInCorrectStatement ) > 1 ) {
+			die( 'Matched multiple Items during reconciliation :(' );
+		}
+
+		// Get our base
+		if ( count( $itemsThatReferenceTheUrlInCorrectStatement ) === 1 ) {
+			$base = $itemsThatReferenceTheUrlInCorrectStatement[0];
+			// XXX: This bit is so annoying...
+			$baseRevId = $repo->getEntityRevisionLookup()
+				->getLatestRevisionId( $base->getId() )
+				->onConcreteRevision( function ( $revId ) {
+					return $revId;
+				} )
+				->onRedirect( function () {
+					throw new \RuntimeException();
+				} )
+				->onNonexistentEntity( function () {
+					throw new \RuntimeException();
+				} )
+				->map();
+		} else {
+			$base = new Item();
+			$baseRevId = false;
+		}
+
+		// Modify the base..
+		// merge labels
+		$base->getFingerprint()->getLabels()->addAll( $inputEntity->getFingerprint()->getLabels()->getIterator() );
+		// merge, descriptions
+		$base->getFingerprint()->getDescriptions()->addAll( $inputEntity->getFingerprint()->getDescriptions()->getIterator() );
+		// merge, aliases
+		// TODO such merging logic would be good to have in DataModel?
+		foreach ( $inputEntity->getFingerprint()->getAliasGroups()->getIterator() as $inputAliasGroup ) {
+			$language = $inputAliasGroup->getLanguageCode();
+			$base->getFingerprint()->getAliasGroups()->setAliasesForLanguage(
+				$language,
+				array_unique( array_merge(
+					$base->getFingerprint()->getAliasGroups()->getByLanguage( $language )->getAliases(),
+					$inputEntity->getFingerprint()->getAliasGroups()->getByLanguage( $language )->getAliases()
+				) )
+			);
+		}
+		// set, statements
+		// TODO use correct statement GUIDs
+		$base->setStatements( $inputEntity->getStatements() );
+
+		// And make the edit
+		$editEntity = $repo->newEditEntityFactory()->newEditEntity(
+			\User::newSystemUser( 'OnOrProtReconciliator' ), // TODO use a real user
+			$base->getId(),
+			$baseRevId
+		);
+		$saveStatus = $editEntity->attemptSave(
+			$base,
+			'Reconciliation Edit',
+			$base->getId() ? null : EDIT_NEW,
+			false // TODO actually do a token check?
+		);
+
+		// Make some sort of response
+		$response = [
+			'success' => $saveStatus->isGood()
+		];
+		return json_encode( $response );
 	}
 
 	public function needsWriteAccess() {
@@ -66,14 +193,14 @@ class EditEndpoint extends SimpleHandler {
 
 	public function getParamSettings() {
 		return [
-			'input' => [
+			'entity' => [
 				self::PARAM_SOURCE => 'post',
-				ParamValidator::PARAM_TYPE => 'upload',
+				//ParamValidator::PARAM_TYPE => 'upload',
 				ParamValidator::PARAM_REQUIRED => true,
 			],
-			'schema' => [
+			'reconcile' => [
 				self::PARAM_SOURCE => 'post',
-				ParamValidator::PARAM_TYPE => 'upload',
+				//ParamValidator::PARAM_TYPE => 'upload',
 				ParamValidator::PARAM_REQUIRED => true,
 			],
 		];
