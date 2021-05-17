ca3f55348fa571f3c34966adcf5c7bccadd4a39b
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2021-05-17 11:09:24 +0200

    Replace die in 'Statements must have property and value keys'
    add test for missing property
    add test for missing value

--- tests/phpunit/unit/InputToEntity/MinimalItemInputTest.php
+++ tests/phpunit/unit/InputToEntity/MinimalItemInputTest.php
@@ -3,6 +3,7 @@
 namespace MediaWiki\Extension\WikibaseReconcileEdit\Tests\Unit\InputToEntity;
 
 use MediaWiki\Extension\WikibaseReconcileEdit\InputToEntity\MinimalItemInput;
+use MediaWiki\Extension\WikibaseReconcileEdit\MediaWiki\ReconciliationException;
 use MediaWiki\Extension\WikibaseReconcileEdit\MediaWiki\ReconciliationService;
 use MediaWiki\Extension\WikibaseReconcileEdit\Wikibase\FluidItem;
 use ValueParsers\StringParser;
@@ -104,6 +105,58 @@ class MinimalItemInputTest extends \MediaWikiUnitTestCase {
 	private function mockReconciliationService() {
 		$mock = $this->createMock( ReconciliationService::class );
 		return $mock;
+	}
+
+	public function testStatementsHavePropertyKey() {
+		$requestEntityMissingProperty = [
+			'wikibasereconcileedit-version' => '0.0.1/minimal',
+			'statements' => [
+				[
+					'value' => 'im-a-string',
+				],
+			],
+		];
+		$sut = new MinimalItemInput(
+			$this->mockPropertyDataTypeLookup(),
+			$this->mockValueParserFactory(),
+			$this->mockReconciliationService()
+		);
+
+		$prop = new PropertyId( 'P23' );
+
+		try {
+			$new = $sut->getItem( $requestEntityMissingProperty, $prop );
+			$this->fail( 'expected ReconciliationException to be thrown' );
+		} catch ( ReconciliationException $rex ) {
+			$this->assertSame( 'wikibasereconcileedit-minimaliteminput-required-keys',
+				$rex->getMessageValue()->getKey() );
+		}
+	}
+
+	public function testStatementsHaveValueKey() {
+		$requestEntityMissingValue = [
+			'wikibasereconcileedit-version' => '0.0.1/minimal',
+			'statements' => [
+				[
+					'property' => 'P23',
+				],
+			],
+		];
+		$sut = $sut = new MinimalItemInput(
+			$this->mockPropertyDataTypeLookup(),
+			$this->mockValueParserFactory(),
+			$this->mockReconciliationService()
+		);
+
+		$prop = new PropertyId( 'P23' );
+
+		try {
+			$new = $sut->getItem( $requestEntityMissingValue, $prop );
+			$this->fail( 'expected ReconciliationException to be thrown' );
+		} catch ( ReconciliationException $rex ) {
+			$this->assertSame( 'wikibasereconcileedit-minimaliteminput-required-keys',
+				$rex->getMessageValue()->getKey() );
+		}
 	}
 
 }
