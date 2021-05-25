e52334f9d7e07c13707a20485327c5eef0afcd05
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2021-05-25 16:07:19 +0200

    Extract ItemReconciler service
    This service wraps a ReconciliationService and SimplePutStrategy and
    extracts the reconciliation URL from an input item before passing it to
    the ReconciliationService and SimplePutStrategy. Its return value is an
    Item and a revision ID, but the meaning is quite different, so we
    introduce a new result class, ReconciledItem, rather than reusing
    ReconciliationServiceItem.
    
    Bug: T283567

--- includes/Reconciliation/ReconciliationServiceItem.php
+++ includes/Reconciliation/ReconciliationServiceItem.php
@@ -5,8 +5,14 @@ namespace MediaWiki\Extension\WikibaseReconcileEdit\Reconciliation;
 use Wikibase\DataModel\Entity\Item;
 
 /**
- * Class that contains the item and revision found by the reconciliation service
- * @see ReconciliationService
+ * An item found by the {@link ReconciliationService}, together with its revision ID.
+ *
+ * This class contains an item as it was found in the database,
+ * or a newly created item if no existing item was found,
+ * in which case the revision ID is false.
+ * It should not be confused with {@link ReconciledItem},
+ * which includes modifications made to the item based on the input.
+ *
  * @license GPL-2.0-or-later
  */
 class ReconciliationServiceItem {
@@ -14,7 +20,7 @@ class ReconciliationServiceItem {
 	/** @var Item */
 	private $item;
 
-	/** @var int|bool */
+	/** @var int|false */
 	private $revision;
 
 	public function __construct( Item $item, $revision ) {
@@ -30,7 +36,10 @@ class ReconciliationServiceItem {
 	}
 
 	/**
-	 * @return bool|int
+	 * The revision from which the item was loaded,
+	 * or false if the item is new.
+	 *
+	 * @return int|false
 	 */
 	public function getRevision() {
 		return $this->revision;
